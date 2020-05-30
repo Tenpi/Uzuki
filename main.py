51e6dfcf2d6cbdb13503c2e9ddcecc9f3a2dadc4
Tenpi <37512637+Tenpi@users.noreply.github.com>
2020-05-29 23:40:08 -0400

    init

---
+++ main.py
@@ -0,0 +1,81 @@
+import os
+import re
+import asyncio
+import praw
+from saucenao_api import SauceNao
+from dotenv import load_dotenv
+load_dotenv()
+
+client_id = os.getenv("REDDIT_CLIENT_ID")
+client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+refresh_token = os.getenv("REDDIT_REFRESH_TOKEN")
+saucenao_key = os.getenv("SAUCENAO_API_KEY")
+user_agent = "Uzuki Bot v1.0 by u/imtenpi"
+
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, refresh_token=refresh_token, user_agent=user_agent)
+
+def get_source(link):
+    """Gets an image source from the Saucenao API."""
+    sauce = SauceNao(api_key=saucenao_key)
+    results = sauce.from_url(link)
+    url = results[0].url 
+    if "pixiv" in url:
+        match = re.search(r"\d+", url)
+        if match:
+            url = f"https://www.pixiv.net/en/artworks/{match.group()}"
+    return f"Title: {results[0].title} Artist: {results[0].author}\n\nSource: {url}" if len(results) else "Sorry, could not find the source of this drawing!"
+
+def duplicate_source(submission, uzuki=False):
+    """Checks if a comment already has a source."""
+    if submission.is_self:
+        return True
+    submission.comments.replace_more(limit=0)
+    submission.comment_sort = "old"
+    comments = submission.comments.list()
+    for comment in comments:
+        match = re.search(r"https?://", comment.body, re.IGNORECASE)
+        if uzuki and match and comment.author.name == "uzukibot":
+            return True
+        elif not uzuki and match:
+            return True
+    return False
+
+async def reply(submission, mention=False):
+    """Replies on a submission with the source."""
+    try:
+        source = get_source(submission.url)
+    except:
+        await asyncio.sleep(30)
+        source = get_source(submission.url)
+    if mention:
+        mention.reply(source)
+    else:
+        submission.reply(source)
+
+async def automatic_reply():
+    """Automatically replies with the source in my subreddits."""
+    while True:
+        print("here2")
+        subreddit = reddit.subreddit("AnimeGirlsInLeggings+KawaiiAnimeGirls+WeebsHideout")
+        for submission in subreddit.stream.submissions():
+            if not duplicate_source(submission):
+                await reply(submission)
+        await asyncio.sleep(120)
+
+async def mention_reply():
+    """Replies with the post source when mentioned."""
+    while True:
+        print("here")
+        for mention in reddit.inbox.mentions(limit=25):
+            submission = mention.submission
+            if not duplicate_source(submission, True):
+                await reply(submission, mention)
+        await asyncio.sleep(120)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(
+        mention_reply(),
+        automatic_reply()
+    ))
+    loop.close()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Uzuki Reddit Bot
+
+This is a simple reddit bot that automatically replies with the source of an image when you mention it (u/uzukibot).
+
+https://www.reddit.com/user/uzukibot
